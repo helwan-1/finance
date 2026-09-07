da38b4cc3a52dda7f2a840deae1b69f8fffcc6d0
helwan-1 <manazelobhur@gmail.com>
2026-09-07 10:23:24 +0300

    chore: remove stray local helper files from UI-closure commit

--- check_ds.sql
+++
@@ -1,3 +0,0 @@
-SELECT ds.id, ds.status,
-  (SELECT count(*) FROM imported_records ir WHERE ir."datasetId"=ds.id) AS imported
-FROM datasets ds ORDER BY ds."createdAt" DESC;
--- show_evidence.sql
+++
@@ -1,6 +0,0 @@
-SELECT ar."resultCode", e."evidenceType", ir."sourceRowNo", ir."rawCells"
-FROM audit_results ar
-JOIN audit_result_evidence e ON e."auditResultId"=ar.id
-JOIN imported_records ir ON ir.id=e."importedRecordId"
-ORDER BY ir."sourceRowNo"
-LIMIT 12;
